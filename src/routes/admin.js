7f9250768874cec91923220bf88c22d83bbd936b
ChesterKing3082003 <46706538+ChesterKing3082003@users.noreply.github.com>
2023-12-24 20:02:36 +0700

    Update admin.js

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -9,17 +9,23 @@ const adminController = require('../controllers/adminController');
 router.get('/showAuthor', adminController.showAuthor);
 router.post('/addAuthor', adminController.addAuthor);
 router.post('/changeAuthorinfo', adminController.changeAuthorinfo);
+router.post('/deleteAuthorinfo', adminController.deleteAuthorinfo);
 
 router.get('/showBranch', adminController.showBranch);
 router.post('/addBranch', adminController.addBranch);
 router.post('/changeBranchinfo', adminController.changeBranchinfo);
+// router.post('/deleteBranchinfo', adminController.deleteBranchinfo);
 
 router.get('/showBook', adminController.showBook);
 router.post('/addBook', adminController.addBook);
 router.post('/changeBookinfo', adminController.changeBookinfo);
-
+router.post('/deleteBookinfo', adminController.deleteBookinfo);
 router.post('/addBookCopies', adminController.addBookCopies);
-router.post('/Manager2Branch', adminController.Manager2Branch);
+
+router.get('/showStaffandManager', adminController.showStaffandManager);
+router.post('/deleteStafffromBranch', adminController.deleteStafffromBranch);
+router.post('/addStaff2Branch', adminController.addStaff2Branch);
+
 // router.post('/addDrinks', adminController.addDrinks);
 
 module.exports = router;
