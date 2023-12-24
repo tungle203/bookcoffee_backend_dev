32125c1c30d7d943734b048c22edc0c44fcaab79
NDAnhTuan <89686737+NDAnhTuan@users.noreply.github.com>
2023-12-24 21:49:08 +0700

    Update manager.js fix showStaff

--- src/routes/manager.js
+++ src/routes/manager.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const verifyToken = require('../middleware/auth');
-// const verifyPermission = require('../middleware/permission')
+
 const managerController = require('../controllers/managerController');
 
-router.get('/showStaff', managerController.showStaff);
+router.get('/showStaff',verifyToken, managerController.showStaff);
 router.get('/showCustomer', managerController.showCustomer);
-router.post('/addStaff', managerController.addStaff);
+router.post('/addStaff',verifyToken, managerController.addStaff);
 router.delete('/deleteStaff', managerController.deleteStaff);
+
+
+router.post('/addBook', managerController.addBook);
+router.post('/changeBookinfo', managerController.changeBookinfo);
+router.post('/addBookCopies',verifyToken, managerController.addBookCopies);
 
 module.exports = router;
